5d0bad9137bb72924d92d951f0117a3c15d7a8cf
pengbin <419995258@qq.com>
2022-01-19 17:30:56 +0800

    修改nacos

--- src/main/java/com/pb/alibabademo/controller/test.java
+++ src/main/java/com/pb/alibabademo/controller/test.java
@@ -1,7 +1,10 @@
 package com.pb.alibabademo.controller;
 
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +14,16 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @RequestMapping("/api/test")
 @Slf4j
+@RefreshScope
 public class test {
+
+
+    @Value("${test:ttt}")
+    private String test;
+
+    @GetMapping("/test")
+    public String test() {
+        System.out.println("example");
+        return test;
+    }
 }
